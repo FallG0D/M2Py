753f61d65f3e1a99829499a76acf42cdad35e0c1
FallG0D <71002332+FallG0D@users.noreply.github.com>
2020-12-06 15:39:24 +0300

    Add files via upload

---
+++ m2-Lesson1.py
@@ -0,0 +1,20 @@
+#import os
+#
+# os.system('dir')
+
+import sys
+
+# print(sys.executable)
+# # sys.exit()
+#
+# nums = 1,2,3
+# idx = 0
+#
+# for i in nums:
+#     print(i)
+
+#
+# from module import *
+sys.path.append("C:\\Users\python\Desktop\\")
+from module import *
+print(sys.path)
